541d7c97545fe5b11768d54816a6f8dda827cf08
ThwinHAung <htoohtoo.mdy.mmr@gmail.com>
2022-11-12 13:39:51 +0630

    hello

--- employee.php
+++ employee.php
@@ -122,7 +122,7 @@ include_once 'layouts/header.php';
                           $count = 1;
                           while($row = mysqli_fetch_array($result) ){
                           $data_id = $row['id'];
-                          $data_name = $row['name'];
+                          $data_name = $row['emp_name'];
                           $data_add = $row['address'];
                           $data_ph = $row['phone_number'];
                           ?>
