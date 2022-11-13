bef3f53b8efc0a35114e6f09b338846408ec1515
AungMyatThu11345 <aungmthu1419@gmail.com>
2022-11-13 12:25:22 +0630

    updateChecker is changed. calDuration is added

--- controller/ReturnController.php
+++ controller/ReturnController.php
@@ -11,16 +11,21 @@ class ReturnController extends Returnn{
         $result = $this->createReturnDetail($return_id,$lent_id,$LentDetail_id,$return_qty,$has_broken,$broken_qty,$price);
         return $result;
     }
-    public function updateChecker($id){
-        $response = $this->changeChecker($id);
+    public function updateChecker($id,$val){
+        $response = $this->changeChecker($id,$val);
         return $response;
     }
     public function calTotalCost($initial_date,$final_date,$lent_price,$qty,$price,$broken_qty,$discount){
         $totalCost=0;
-        $interval=date_diff($final_date,$initial_date);
-        $duration = ($interval->y*365)+($interval->m*30)+($interval->d);
+        $duration =$this->calDuration($initial_date,$final_date);
         $totalCost+=($lent_price*$qty*$duration)+($price*$broken_qty)-$discount;
         return $totalCost;
+    }
+    public function calDuration($initial_date,$final_date){
+        $interval=date_diff($final_date,$initial_date);
+        $duration = ($interval->y*365)+($interval->m*30)+($interval->d);
+        $duration = intval($duration);
+        return $duration;
     }
 }
 ?>
